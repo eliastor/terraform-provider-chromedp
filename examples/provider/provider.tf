1d3f7288f5d5b47729cd84e85d83248567590438
eliastor <eliastor@users.noreply.github.com>
2023-08-22 22:10:55 +0300

    enhancements and fixes in docs

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,12 +1,11 @@
 terraform {
   required_providers {
     chromedp = {
-      source = "hashicorp.com/eliastor/chromedp"
+      source = "eliastor/chromedp"
     }
   }
 }
 
 provider "chromedp" {
-  # for an instance: podman run --rm -d -p 3000:3000 docker.io/browserless/chrome:latest
   endpoint = "ws://localhost:3000"
 }
